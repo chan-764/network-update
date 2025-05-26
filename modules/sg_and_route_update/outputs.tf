e43c9f7ec8599584e5a8e76280173db632bcc482
chan-764 <chandanuikey19@gmail.com>
2025-05-26 09:15:32 +0530

    Create outputs.tf

---
+++ modules/sg_and_route_update/outputs.tf
@@ -0,0 +1,9 @@
+output "security_group_id" {
+  description = "The updated security group ID"
+  value       = var.sg_id
+}
+
+output "route_table_id" {
+  description = "The updated route table ID"
+  value       = var.route_table_id
+}
