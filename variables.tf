0b61ce65743477cde8f37ef72d8fa2dd3044ea71
chan-764 <chandanuikey19@gmail.com>
2025-05-28 18:35:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,1 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
 
-variable "sg_id" {
-  description = "Security Group ID"
-  type        = string
-}
-
-variable "route_table_id" {
-  description = "Route Table ID"
-  type        = string
-}
-
-variable "igw_id" {
-  description = "Internet Gateway ID"
-  type        = string
-}
